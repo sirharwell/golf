ea7c5e1533cd18e710cd3b2b0411436453538491
Ian Harwell <sirharwell@gmail.com>
2018-02-19 16:32:11 -0700

    ~"multiples"

--- multiples.rb
+++ multiples.rb
@@ -1,2 +1,7 @@
 # The method takes in a number and a max value
 # then finds all multiples of the number up to the max value.
+def multiples(n,m)
+   (n..m).step(n) { |i| p i }
+ end
+
+ multiples(5,100)
