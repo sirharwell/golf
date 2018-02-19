e8a363a98bb718e82e499df5f2aa33abbf5153f9
Ian Harwell <sirharwell@gmail.com>
2018-02-19 16:36:33 -0700

    cipher1

---
+++ cipher.rb
@@ -0,0 +1,5 @@
+# Take in a string and an offset, encrypt the string by moving letters over
+ # by the offset and return the encrypted string (e.g “ab”, 3 would return “de”).
+ # When you hit Z loop back to A.
+
+ 
