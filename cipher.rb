370f65047cdc477f233b6feefae4822ee90fd0c1
Ian Harwell <sirharwell@gmail.com>
2018-02-19 17:31:00 -0700

    cipher2

--- cipher.rb
+++ cipher.rb
@@ -2,16 +2,18 @@
  # by the offset and return the encrypted string (e.g “ab”, 3 would return “de”).
  # When you hit Z loop back to A.
 
-# 
+#
 # a = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
 # b = gets.to_i
 # puts a.rotate(b)
 
 
-def caesar_cipher(string, shift = 1)
-  alphabet   = Array('a'..'z')
-  encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
-  string.chars.map { |c| encrypter.fetch(c, " ") }
+def cipher(s)
+  puts "code number"
+  a  = Array('a'..'z')
+  e  = Hash[a.zip(a.rotate(gets.to_i))]
+  s.chars.map { |c| e.fetch(c, " ") }
 end
 
-p caesar_cipher("testing").join
+puts 'what to encode'
+p cipher(gets.downcase).join
